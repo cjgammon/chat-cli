29b3458ec9ab014fc48f9cae406e683baaa9f29c
cjgammon <cj.gammon@gmail.com>
2024-03-12 00:27:09 -0500

    add files

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-# chat-cli
+# trm-cli
+
+This is a command line tool for using AI to take human readable text and convert it into shell commands to run in terminal.
+
+## usage
+
+```
+llm
+```
+
+## installation
+
+To install, from inside directory
+
+add `.env` file to `bin` with:
+
+```
+OPENAI_API_KEY=<YOUR_API_KEY>
+```
+
+Then run:
+
+```
+npm install -g
+```
+
+NOTE:: may need sudo
---
+++ bin/index.js
@@ -0,0 +1,175 @@
+#!/usr/bin/env node
+import { exec } from "child_process";
+
+import _yargs from "yargs";
+import { hideBin } from "yargs/helpers";
+
+import openAI from "openai";
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+import dotenv from "dotenv";
+import path from "path";
+
+import chalk from "chalk";
+import { confirm, input } from "@inquirer/prompts";
+
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+dotenv.config({
+  path: `${__dirname}/.env`,
+});
+
+const yargs = _yargs(hideBin(process.argv));
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
+
+const openai = new openAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const SYSTEM_PROMPT = ``;
+
+yargs
+  .option("model", {
+    alias: "m",
+    describe: "Model to use",
+    type: "string",
+  })
+  .option("temp", {
+    alias: "t",
+    describe: "Temperature to use",
+    type: "number",
+  })
+  .command({
+    command: "gemini",
+    describe: "Use Gemini",
+    handler: () => {
+      runGemini();
+    },
+  })
+  .command({
+    command: "openai",
+    describe: "Use OpenAI",
+    handler: () => {
+      runOpenai();
+    },
+  })
+  .command({
+    command: "$0",
+    describe: "default",
+    handler: () => {
+      runOpenai();
+    },
+  })
+  .help()
+  .parse();
+
+async function runGemini() {
+  const modelName = "gemini-pro";
+  const model = genAI.getGenerativeModel({ model: modelName });
+
+  let prompt;
+  try {
+    prompt = await input({ message: `${modelName}:` });
+  } catch (error) {
+    //console.log(chalk.red("Error: ", error));
+    return;
+  }
+
+  if (!prompt || prompt === "") {
+    console.log(chalk.red("No prompt provided!"));
+    return;
+  }
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+
+  console.log(chalk.green(text));
+
+  await checkCopy(text);
+
+  runGemini();
+}
+
+async function runOpenai() {
+  let temperture = 0.5;
+  let model = "gpt-3.5-turbo";
+
+  const modelArg = yargs.argv.model;
+  if (modelArg == "4") {
+    model = "gpt-4-turbo-preview";
+  }
+
+  const tempArg = yargs.argv.temp;
+  if (tempArg) {
+    temperture = tempArg;
+  }
+
+  let prompt;
+  try {
+    prompt = await input({ message: `${model}:` });
+  } catch (error) {
+    //console.log(chalk.red("Error: ", error));
+    return;
+  }
+
+  if (!prompt || prompt === "") {
+    console.log(chalk.red("No prompt provided!"));
+    return;
+  }
+
+  const chatCompletion = await openai.chat.completions.create({
+    model,
+    temperature,
+    top_p: 1,
+    messages: [
+      {
+        role: "system",
+        content: SYSTEM_PROMPT,
+      },
+      {
+        role: "user",
+        content: prompt,
+      },
+    ],
+  });
+
+  const responseMessage = chatCompletion.choices[0].message;
+
+  console.log(chalk.green(responseMessage.content));
+
+  await checkCopy(responseMessage.content);
+
+  runOpenai();
+}
+
+async function checkCopy(prompt) {
+  let copy;
+  try {
+    copy = await confirm({
+      message: "copy?",
+      default: false,
+    });
+  } catch (error) {
+    console.log(chalk.red("Error: ", error));
+  }
+
+  if (copy) {
+    exec(`echo ${prompt} | pbcopy`, (error, stdout, stderr) => {
+      if (error) {
+        console.log(chalk.red("Error: ", error));
+        return;
+      }
+      if (stderr) {
+        console.log(chalk.red("stderr: ", stderr));
+        return;
+      }
+
+      console.log(chalk.green("copied!"));
+    });
+  }
+}
